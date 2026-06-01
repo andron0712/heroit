8caacd1d6a88c821a8a1ec449aa96f6a296a95fa
andron0712 <andriygryban07@gmail.com>
2026-06-01 14:04:37 +0300

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header style={{ textAlignment: 'center', marginBottom: '20px', borderBottom: '2px solid #eee', paddingBottom: '10px' }}>
+      <h1 style={{ color: '#2c3e50', margin: 0 }}>Мій Список Задач (React To-Do)</h1>
+    </header>
+  );
+}
+
+export default Header;
